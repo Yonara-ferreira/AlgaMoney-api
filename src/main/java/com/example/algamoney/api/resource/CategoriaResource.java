657efb5f4c1407f48f33dd341007be150cd742ae
yonara ferreira <yonara.fd@gmail.com>
2023-03-23 00:04:35 -0300

    Cadastrando nova categoria e retornando 404 caso codigo nao exista

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,14 +1,23 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/categorias")
@@ -22,5 +31,23 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
+	@PostMapping
+	public ResponseEntity<Categoria> criar( @RequestBody Categoria categoria, HttpServletRequest response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setAttribute("Location", uri.toASCIIString());
+		
+		 return ResponseEntity.created(uri).body(categoriaSalva);
+	
+	}
+	
+	@GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+        Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+
+        return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+    }
 	
 }
