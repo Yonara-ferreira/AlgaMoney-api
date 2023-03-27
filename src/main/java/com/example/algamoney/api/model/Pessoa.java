18fb6529bb79996bf53e4f660a4acc4d3bb04209
yonara ferreira <yonara.fd@gmail.com>
2023-03-27 15:07:49 -0300

    PARCIAL: desafio cadastro de pessoas 1/3

---
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -0,0 +1,73 @@
+package com.example.algamoney.api.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigo;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private Boolean ativo;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return codigo == other.codigo;
+	}
+	
+}
---
+++ src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -0,0 +1,26 @@
+CREATE TABLE pessoa (
+	codigo		BIGINT(20) 	PRIMARY KEY AUTO_INCREMENT,
+	nome 		VARCHAR(50) NOT NULL,
+	ativo 		BOOLEAN 	NOT NULL,
+	logradouro  VARCHAR(60),
+	numero		VARCHAR(10),
+	complemento	VARCHAR(50),
+	bairro		VARCHAR(50),
+	cep			VARCHAR(10),
+	cidade		VARCHAR(50),
+	estado		VARCHAR(2)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO pessoa (nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado) 
+VALUES 
+('João da Silva', 1, 'Rua das Flores', 123, 'Apto 101', 'Centro', '12345-678', 'São Paulo', 'SP'),
+('Maria Souza', 1, 'Avenida dos Pássaros', 456, '', 'Jardim Primavera', '54321-098', 'Rio de Janeiro', 'RJ'),
+('José Pereira', 0, 'Rua das Árvores', 789, '', 'Vila Verde', '45678-912', 'Belo Horizonte', 'MG'),
+('Ana Oliveira', 1, 'Rua das Pedras', 1010, '', 'Costa Azul', '23456-789', 'Salvador', 'BA'),
+('Fernanda Santos', 1, 'Avenida dos Girassóis', 222, 'Casa 2', 'Jardim das Flores', '87654-321', 'Curitiba', 'PR'),
+('Ricardo Santos', 0, 'Rua das Acácias', 567, 'Casa 1', 'Jardim das Oliveiras', '34567-890', 'Campinas', 'SP'),
+('Pedro Lima', 0, 'Rua dos Pinheiros', 987, 'Bloco C', 'Jardim Europa', '01234-567', 'São Paulo', 'SP'),
+('Carla Fernandes', 1, 'Avenida dos Coqueiros', 1010, 'Sala 2', 'Praia do Forte', '78901-234', 'Natal', 'RN'),
+('Pedro Lima', 0, 'Rua dos Pinheiros', 987, 'Bloco C', 'Jardim Europa', '01234-567', 'São Paulo', 'SP'),
+('Lucas Alves', 1, 'Avenida dos Sabiás', 246, 'Casa 3', 'Jardim das Flores', '65432-109', 'Belo Horizonte', 'MG')
+('Mariana Souza', 1, 'Rua das Laranjeiras', 321, '', 'Centro', '45678-901', 'Florianópolis', 'SC'),;
