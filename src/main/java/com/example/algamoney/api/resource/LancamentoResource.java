8e6222ec06fa5d7fef57516d29ecd18d67b522c1
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2023-07-24 12:42:43 -0300

    Created method delete 'lancamento'

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -11,13 +11,17 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
@@ -70,6 +74,19 @@ public class LancamentoResource {
 
         return ResponseEntity.badRequest().body(erros);
     }
-	
-	
+    
+    @DeleteMapping(value = "deletarLancamento")
+    @ResponseBody
+    public ResponseEntity<String> deletarLancamento(@RequestParam(name = "codigo") Long codigo){
+    	if (isLancamentoExistente(codigo)) {
+			lancamentosRepository.deleteById(codigo);
+			return ResponseEntity.ok("Lancamento foi deletado com sucesso");
+		}else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O lancamento nao foi encontrado");
+		}
+    }
+    
+    private Boolean isLancamentoExistente(Long codigo){
+        return lancamentosRepository.existsById(codigo);
+    }
 }
